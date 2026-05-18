6050e3d94e7dce696f934ae8f65a9f299a7520b1
Shamsul <48136141+shkms11@users.noreply.github.com>
2026-05-18 13:53:39 +0600

    fix path error

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,5 +1,5 @@
 import { Provider } from "react-redux";
-import { store } from "@";
+import { store } from "@/app/store";
 import { BrowserRouter } from "react-router-dom";
 
 export default function Providers({ children }: { children: React.ReactNode }) {
--- src/app/store.ts
+++ src/app/store.ts
@@ -1,5 +1,5 @@
 import { configureStore } from "@reduxjs/toolkit";
-import { api } from "@";
+import { api } from "@/shared/api/baseApi";
 
 export const store = configureStore({
   reducer: {
